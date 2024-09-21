2c03fead35260f61ccbc51f386b4a57f06cd346f
Punya Modi <68418104+punyamodi@users.noreply.github.com>
2024-09-21 19:54:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import pickle
 import streamlit as st
 import requests
 import pandas as pd
+import os
 
 # Function to download a file from a URL
 def download_file(url, destination):
     response = requests.get(url)
-    with open(destination, 'wb') as file:
-        file.write(response.content)
+    if response.status_code == 200:
+        with open(destination, 'wb') as file:
+            file.write(response.content)
+    else:
+        st.error("Failed to download the file from the provided URL.")
+        return False
+    return True
 
 # Google Drive link to 'movies_dict.pkl' (replace with your actual link)
 movies_dict_url = 'https://drive.google.com/uc?export=download&id=1HCNOOnP4NgsKPb6qGqfbr-ekjx08VsvI'
@@ -16,14 +22,19 @@ movies_dict_url = 'https://drive.google.com/uc?export=download&id=1HCNOOnP4NgsKP
 movies_dict_path = 'movies_dict.pkl'
 
 # Download the file from Google Drive
-download_file(movies_dict_url, movies_dict_path)
+if download_file(movies_dict_url, movies_dict_path):
+    try:
+        # Load the downloaded pickle file
+        with open(movies_dict_path, 'rb') as f:
+            similarity = pickle.load(f)
+    except pickle.UnpicklingError:
+        st.error("The downloaded file is not a valid pickle file.")
+else:
+    st.error("Could not download the file. Please check the URL.")
 
-# Load the downloaded pickle file
+# Assuming the 'movie_list.pkl' is locally available
 with open('movie_list.pkl', 'rb') as f:
-    movies = pd.read_pickle("movie_list.pkl")
-
-with open(movies_dict_path, 'rb') as f:
-    similarity = pickle.load(f)
+    movies = pd.read_pickle(f)
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=bfb2634dae4592761658dba50f11ec8f&language=en-US".format(movie_id)
